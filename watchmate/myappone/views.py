407729803a2e0d403f18b2a31f5f5a5823140916
karan-310 <karan.kesarwani310.com>
2023-08-26 15:25:33 +0530

    commit:13

--- watchmate/myappone/views.py
+++ watchmate/myappone/views.py
@@ -1,32 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from myappone.models import Movie
+# from django.shortcuts import render
+# from django.http import HttpResponse, JsonResponse
+# from myappone.models import Movie
 
-# Create your views here.
+#  Create your views here.
+
+#  def movie_list(request):
+#      movies = Movie.objects.all()
+#      return HttpResponse(movies.values())
+
+# #-------------------------------------------------------------------------------------------------------------------------------------------
 
 # def movie_list(request):
 #     movies = Movie.objects.all()
-#     return HttpResponse(movies.values())
-
-#-------------------------------------------------------------------------------------------------------------------------------------------
-
-def movie_list(request):
-    movies = Movie.objects.all()
     
-    data = {
-        'movies':list(movies.values())
-    }
+#     data = {
+#         'movies':list(movies.values())
+#     }
     
-    return JsonResponse(data)
+#     return JsonResponse(data)
 
 
-def movie_details(requests, pk):
-    movie = Movie.objects.get(pk=pk)
+# def movie_details(requests, pk):
+#     movie = Movie.objects.get(pk=pk)
     
-    data = {
-        'movie':movie.name,
-        'descriptions':movie.description,
-        'active':movie.active
-    }
+#     data = {
+#         'movie':movie.name,
+#         'descriptions':movie.description,
+#         'active':movie.active
+#     }
     
-    return JsonResponse(data)
+#     return JsonResponse(data)
