660a3b7297f9ae150696aefde466e2cf5848f7c1
karan-310 <karan.kesarwani310.com>
2023-08-27 15:55:41 +0530

    Commit:20

--- watchmate/myappone/api/views.py
+++ watchmate/myappone/api/views.py
@@ -2,47 +2,54 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from myappone.models import Movie
 from myappone.api.serializers import MovieSerializer
+from rest_framework import status
 
 
 # ---------------------------------------------------------------------------------------------------------------------
 # by default: set for 'GET' request
 # 'many=True' is used because of extracting more than one object
 
-@api_view(['GET', 'POST'])                                                 
+@api_view(['GET', 'POST'])
 def movie_list(request):
     if request.method == 'GET':
         movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)         
-        return Response(serializer.data)
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
 
     if request.method == 'POST':
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors)
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 # by default: set for 'GET' request
 
-@api_view(['GET', 'PUT', 'DELETE'])                                                 
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
-    
+
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
-        serializer = MovieSerializer(movie)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = MovieSerializer(movie, many=True)
         return Response(serializer.data)
-    
-    if request.method == 'PUT':
-        serializer = MovieSerializer(movie, data=request.data)
+
+    if request.method == 'POST':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer( movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == 'DELETE':
-        pass
-    
-    
+        if request.method == 'DELETE':
+            movie = Movie.objects.get(pk=pk)
+            movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
